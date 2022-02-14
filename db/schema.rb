aae472ba7a1a2516533aaa10e1500a1fab0b7e03
kesleymartins <kesley.martins@vivaldi.net>
2022-02-14 02:57:44 -0300

    create migration for views field inside posts and change the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_13_031333) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_14_054830) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
