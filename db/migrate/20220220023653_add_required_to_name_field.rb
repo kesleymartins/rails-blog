deb133d6dbd593e02d442732496b4bd0b02330c7
kesleymartins <kesley.martins@vivaldi.net>
2022-02-20 14:03:33 -0300

    make name field not null

---
+++ db/migrate/20220220023653_add_required_to_name_field.rb
@@ -0,0 +1,5 @@
+class AddRequiredToNameField < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :users, :name, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_17_235005) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_20_023653) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -76,7 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_235005) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
