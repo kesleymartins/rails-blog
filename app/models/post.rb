8e8b5b975be7053ba1bf42935d235e75b1a6dee3
kesleymartins <kesley.martins@vivaldi.net>
2022-02-23 23:44:33 +0000

    add validation on create post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { in: 3..40  }
+    validates :body, presence: true, length: { minimum: 5 }
+    
     belongs_to :user
 
     has_many :comments, dependent: :destroy
