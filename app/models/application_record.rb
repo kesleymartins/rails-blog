61a441d669d04596589aaf6d3ad41888a9d4da26
kesleymartins <kesley.martins@vivaldi.net>
2022-02-24 23:28:05 +0000

    move scope latests from post model to aplication_record to work with posts and comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  scope :latests, -> { order('created_at DESC')}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,4 @@ class Post < ApplicationRecord
     belongs_to :user
 
     has_many :comments, dependent: :destroy
-
-    scope :latests, -> { order('created_at DESC')}
 end
