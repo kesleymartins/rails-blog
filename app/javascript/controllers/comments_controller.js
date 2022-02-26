50a9c265f2e7dc27301b52cfb61ff6e568614fbf
kesleymartins <kesley.martins@vivaldi.net>
2022-02-26 22:34:39 +0000

    toggle the comment and form to display: none

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -3,7 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   initialize() {}
   connect() {}
-  toggleForm() {
-    console.log('edit buttos was clicked!')
+
+  toggleForm(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    
+    const formId = event.params['form']
+    const commentBodyId = event.params['body']
+  
+    const form = document.getElementById(formId)
+    const commentBody = document.getElementById(commentBodyId)
+
+    form.classList.toggle('d-none')
+    commentBody.classList.toggle('d-none')
   }
 }
