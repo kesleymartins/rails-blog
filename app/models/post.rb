e3b38c28c227d9614c9d7b7130f0c5da12b7fa03
kesleymartins <kesley.martins@vivaldi.net>
2022-03-01 20:52:27 -0300

    change the title max length to 35

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { in: 3..40  }
+    validates :title, presence: true, length: { in: 3..35  }
     validates :body, presence: true, length: { minimum: 5 }
     
     belongs_to :user
