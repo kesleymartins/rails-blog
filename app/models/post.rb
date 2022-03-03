1775249d951ffc9271b8b1e02e90c0fb814e0999
kesleymartins <kesley.martins@vivaldi.net>
2022-03-02 22:49:46 -0300

    add relation to the notification model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,7 @@ class Post < ApplicationRecord
     belongs_to :user
 
     has_many :comments, dependent: :destroy
+
+    has_noticed_notifications model_name: 'Notification'
+    has_many :notifications, through :user, dependent: :destroy
 end
