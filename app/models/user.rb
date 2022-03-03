46aa4bc2a21ae676e378f50b9cca47516f2b65cd
kesleymartins <kesley.martins@vivaldi.net>
2022-03-02 22:30:44 -0300

    add relation has_many notification to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :notification, as: :recipient, dependent: :destroy
 
 end
