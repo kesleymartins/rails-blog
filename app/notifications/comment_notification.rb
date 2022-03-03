6c35c8978919d1303fce51cded6a2d17f782b4da
kesleymartins <kesley.martins@vivaldi.net>
2022-03-03 13:44:50 -0300

    add message to notification

--- app/notifications/comment_notification.rb
+++ app/notifications/comment_notification.rb
@@ -17,10 +17,14 @@ class CommentNotification < Noticed::Base
 
   # Define helper methods to make rendering easier.
   #
-  # def message
-  #   t(".message")
-  # end
-  #
+  def message
+    @post = Post.find(params[:comment][:post_id])
+    @comment = Comment.find(params[:comment][:post_id])
+    @user = User.find(@comment.user_id)
+
+    "#{@user.email} commented on #{@post.title.truncate(10)}"
+  end
+
   def url
     post_path(Post.find(params[:comment][:post_id]))
   end
