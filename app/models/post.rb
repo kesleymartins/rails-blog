23262a6c07574acacd26be2cd1fa814430cbc9a3
kesleymartins <kesley.martins@vivaldi.net>
2022-03-03 21:40:27 -0300

    fix: post relation to notification

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
     validates :title, presence: true, length: { in: 3..35  }
     validates :body, presence: true, length: { minimum: 5 }
-    
+
     belongs_to :user
 
     has_many :comments, dependent: :destroy
 
     has_noticed_notifications model_name: 'Notification'
-    has_many :notifications, through :user, dependent: :destroy
+    has_many :notifications, through: :user, dependent: :destroy
 end
