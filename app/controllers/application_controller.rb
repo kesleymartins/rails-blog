ddc595e9c0dfd930222968e729eff146370824c1
kesleymartins <kesley.martins@vivaldi.net>
2022-03-03 22:04:21 -0300

    set notifications if is current user logged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
+
+    before_action :set_notifications, if: :current_user
+
+    private
+
+    def set_notifications
+      notifications = Notification.where(recipient: current_user).newest_first
+      @unread = notifications.unread
+      @read = notifications.read
+    end
 end
