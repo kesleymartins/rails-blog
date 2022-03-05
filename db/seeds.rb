d2f73dec471bfaf3cc7c8f69e0106cc057762cdf
kesleymartins <kesley.martins@vivaldi.net>
2022-03-05 11:28:41 -0300

    add info mensage for steps

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "creating users..."
 10.times {
     User.create(email: Faker::Internet.email, name: Faker::Name.name, password: '123456', password_confirmation: '123456')
 }
 
-users = User.all
-
+puts "creating posts..."
 25.times {
-    users.each { |user|
+    User.all.each { |user|
         Post.create(title: Faker::Fantasy::Tolkien.poem, body: Faker::Quotes::Shakespeare.hamlet_quote, user_id: user.id)
-    }  
+    }
 }
