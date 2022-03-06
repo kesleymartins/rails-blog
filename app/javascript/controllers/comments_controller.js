35eadc0c3fe13217849d3fb5bf12f9846c0bc806
kesleymartins <kesley.martins@vivaldi.net>
2022-03-06 17:34:03 -0300

    change button text and color if is editing comment

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,20 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  initialize() {}
-  connect() {}
+  initialize() {
+  }
+
+  connect() {
+  }
 
   toggleForm(event) {
     event.preventDefault()
     event.stopPropagation()
-    
+
     const formId = event.params['form']
     const commentBodyId = event.params['body']
-  
+    const editBtnId = event.params['editBtn']
+
     const form = document.getElementById(formId)
     const commentBody = document.getElementById(commentBodyId)
+    const editBtn = document.getElementById(editBtnId)
 
     form.classList.toggle('d-none')
     commentBody.classList.toggle('d-none')
+
+    this.toggleEditButton(editBtn)
+  }
+
+  toggleEditButton(editButton) {
+    editButton.innerText = editButton.innerText === 'Edit' ? 'Cancel' : 'Edit'
+
+    this.toggleEditButtonClass(editButton)
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle('btn-warning')
+    editButton.classList.toggle('btn-secondary')
   }
 }
