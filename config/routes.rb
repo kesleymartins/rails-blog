831dec6ebee737cc3777d5526b37cbe0ac59fb09
kesleymartins <kesley.martins@vivaldi.net>
2022-03-22 23:25:52 -0300

    create routes for comments, users and posts section

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   get 'user/:id', to: 'user#profile', as: 'user'
   get 'about', to: 'pages#about'
-  get 'admin', to: 'admin#index'
 
+  get 'admin', to: 'admin#index'
+  get 'admin/posts', to: 'admin#posts'
+  get 'admin/comments', to: 'admin#comments'
+  get 'admin/users', to: 'admin#users'
+  
   resources :posts do
     resources :comments
   end
