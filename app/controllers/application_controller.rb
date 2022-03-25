cc02acd5beb9171d53f1736650ff72d0690aff3e
kesleymartins <kesley.martins@vivaldi.net>
2022-03-24 22:00:52 -0300

    add alert when user is not authorized to do action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
     include Pagy::Backend
 
     before_action :set_notifications, if: :current_user
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
     private
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to(request.referrer || root_path)
+    end
 
     def set_notifications
       notifications = Notification.where(recipient: current_user).newest_first
