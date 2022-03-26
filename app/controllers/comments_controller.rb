4c29f27730af60f1d4407fa8e88899c4924e4363
kesleymartins <kesley.martins@vivaldi.net>
2022-03-26 14:02:25 -0300

    change identation 4 -> 2 and add authorization for update and destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,43 +1,48 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_post
+    before_action :set_comment, except: %i[ create ]
+    before_action :authorize_action, except: %i[ create ]
 
     def create
-        @comment = @post.comments.create(comment_params)
-        @comment.user = current_user
+      @comment = @post.comments.create(comment_params)
+      @comment.user = current_user
 
-        if @comment.save
-            redirect_to post_path(@post), notice: "Comment has been created"
-        else
-            redirect_to post_path(@post), alert: "Comment has not been created"
-        end
-        
+      if @comment.save
+        redirect_to post_path(@post), notice: "Comment has been created"
+      else
+        redirect_to post_path(@post), alert: "Comment has not been created"
+      end
     end
 
     def destroy
-        @comment = @post.comments.find(params[:id])
-        @comment.destroy
-
-        redirect_to post_path(@post)
+      @comment.destroy
+      redirect_to post_url(@post), notice: "comment has been deleted"
     end
 
     def update
-        @comment = @post.comments.find(params[:id])
-        
-        if @comment.update(comment_params) then
-            redirect_to post_url(@post), notice: "comment has been updated"
-        else
-            redirect_to post_url(@post), alert: "Comment has not been updated"
-        end
+      if @comment.update(comment_params) then
+        redirect_to post_url(@post), notice: "comment has been updated"
+      else
+        redirect_to post_url(@post), alert: "Comment has not been updated"
+      end
     end
 
     private
 
+    def authorize_action
+      authorize @comment
+    end
+
+    def set_comment
+      @comment = @post.comments.find(params[:id])
+    end
+
     def set_post
-        @post = Post.find(params[:post_id])
+      @post = Post.find(params[:post_id])
     end
 
     def comment_params
-        params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body)
     end
 end
