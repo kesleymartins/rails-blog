daa6a1806940b5360af50f3e66b0d151160845b0
kesleymartins <kesley.martins@vivaldi.net>
2022-04-10 14:53:15 -0300

    factory bot config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # FactoryBot config
+  config.include FactoryBot::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
